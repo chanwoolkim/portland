286f1ebab13c5145ac7fb34928e341f2b93fb441
chanwoolkim <chanwoolkim@uchicago.edu>
2023-09-21 21:13:18 -0500

    Adding prices

--- databuild/bill_usage_clean.R
+++ databuild/bill_usage_clean.R
@@ -1,9 +1,11 @@
 load(file=paste0(working_data_dir, "/analysis_info_large.RData.gz"))
-View(usage_info %>% group_by(ACCOUNT_NO, BILL_RUN_DT, REPORT_CONTEXT) %>% mutate(nrow=n()) %>% ungroup() %>% filter(nrow>1, is.na(BC_ACTIVE_DAYS), BC_DETAIL_PRORATED_YN))
+
 usage_info <- usage_info %>%
   mutate(BILL_PRINT_CD=trimws(BILL_PRINT_CD),
          REPORT_CONTEXT=trimws(REPORT_CONTEXT),
-         WEIGHT=BC_ACTIVE_DAYS/BC_STANDARD_DAYS) %>%
+         WEIGHT=ifelse(BC_DETAIL_PRORATED_YN,
+                       BC_ACTIVE_DAYS/BC_STANDARD_DAYS,
+                       1)) %>%
   group_by(ACCOUNT_NO, BILL_RUN_DT) %>%
   summarise(usage_bill_amount=sum(BC_DETAIL_AMT, na.rm=TRUE),
             usage_bill_water_cons=sum(BC_DETAIL_AMT[BILL_PRINT_CD=="WATER"], na.rm=TRUE),
@@ -12,24 +14,35 @@ usage_info <- usage_info %>%
             sewer_cons=sum(CONS_LEVEL_AMT[REPORT_CONTEXT=="SCONS"], na.rm=TRUE),
             
             water_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="WCONS"],
-                                          na.rm=TRUE)
-            WCONS - variable price
-            SCONS - variable price
-            WBASE - fixed price
-            SBASE - fixed price
-            STORM - fixed price
-            STORM_PHS - fixed price
-            SEWER_PHS - variable price (SEWER)
-            BOD - variable price (SEWER)
-            TSS - variable price (SEWER)
-            CLEANRIVER - variable price (SEWER)
-            CLNRVRCRD - variable price (SEWER)
-            SWR DONAT - donation (SEWER)
-            LINCSEWER - fixed price (SEWER)
-            LINC - fixed price (WATER)) %>%
+                                          WEIGHT[REPORT_CONTEXT=="WCONS"], na.rm=TRUE),
+            sewer_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="SCONS"],
+                                          WEIGHT[REPORT_CONTEXT=="SCONS"], na.rm=TRUE),
+            water_fixed_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="WBASE"],
+                                          WEIGHT[REPORT_CONTEXT=="WBASE"], na.rm=TRUE),
+            sewer_fixed_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="SBASE"],
+                                          WEIGHT[REPORT_CONTEXT=="SBASE"], na.rm=TRUE),
+            storm_fixed_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="STORM"],
+                                            WEIGHT[REPORT_CONTEXT=="STORM"], na.rm=TRUE),
+            storm_phs_fixed_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="STORM_PHS"],
+                                            WEIGHT[REPORT_CONTEXT=="STORM_PHS"], na.rm=TRUE),
+            sewer_phs_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="SEWER_PHS"],
+                                            WEIGHT[REPORT_CONTEXT=="SEWER_PHS"], na.rm=TRUE),
+            bod_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="BOD"],
+                                                WEIGHT[REPORT_CONTEXT=="BOD"], na.rm=TRUE),
+            tss_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="TSS"],
+                                        WEIGHT[REPORT_CONTEXT=="TSS"], na.rm=TRUE),
+            cleanriver_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="CLEANRIVER"],
+                                        WEIGHT[REPORT_CONTEXT=="CLEANRIVER"], na.rm=TRUE),
+            clnrvrcrd_var_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="CLNRVRCRD"],
+                                               WEIGHT[REPORT_CONTEXT=="CLNRVRCRD"], na.rm=TRUE),
+            swr_donation_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="SWR DONAT"],
+                                              WEIGHT[REPORT_CONTEXT=="SWR DONAT"], na.rm=TRUE),
+            linc_sewer_fixed_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="LINCSEWER"],
+                                             WEIGHT[REPORT_CONTEXT=="LINCSEWER"], na.rm=TRUE),
+            linc_water_fixed_price=weighted.mean(BC_DETAIL_RATE[REPORT_CONTEXT=="LINC"],
+                                             WEIGHT[REPORT_CONTEXT=="LINC"], na.rm=TRUE)) %>%
   ungroup() %>%
-  mutate(BILL_RUN_DT=mdy(BILL_RUN_DT)) %>%
-  select()
+  mutate(BILL_RUN_DT=mdy(BILL_RUN_DT))
 
 financial_info <- financial_info %>%
   mutate(ACCOUNT_NO=as.character(SS_ACCOUNT_NO),
