d5035b8e1a3c659f9af9614103f2da084eb254dc
chanwoolkim <chanwoolkim@uchicago.edu>
2024-07-27 13:35:24 -0500

    Table for FA and honored citizens

--- scratch/recent_delinquent.R
+++ scratch/recent_delinquent.R
@@ -19,77 +19,81 @@ output_dir <- paste0(code_dir, "/output")
 source(paste0(code_dir, "/utilities/preliminary.R"))
 
 # Load data
-load(paste0(working_data_dir, "/portland_panel_estimation_old.RData"))
-portland_panel_estimation_old <- portland_panel_estimation
+load(paste0(working_data_dir, "/portland_transunion.RData"))
 
-load(paste0(working_data_dir, "/portland_panel_estimation.RData"))
+portland_panel_2024q2 <- portland_transunion %>%
+  filter(year(bill_date)==2024, quarter(bill_date)==2)
 
-# Homogenise two datasets
-portland_panel_estimation_old <- portland_panel_estimation_old %>%
-  transmute(account_number=as.numeric(ACCOUNT_NO),
-            person_number=PERSON_NO,
-            due_date=DUE_DT,
-            bill_date=BILL_RUN_DT,
-            bill_year, previous_bill, total_payments, leftover_debt, current_bill,
-            source_code=SOURCE_CD,
-            type_code=BILL_TP,
-            is_off_cycle=OFF_CYCLE_YN,
-            start_date=PERIOD_FROM_DT,
-            end_date=PERIOD_TO_DT,
-            cycle_code=CYCLE_CD,
-            payment_arrange, payment_arrange_status, financial_assist, cutoff,
-            usage_bill_amount, water_cons, sewer_cons,
-            bill_penalty, bill_bankrupt, bill_donate, bill_leaf,
-            price_water, price_sewer, price_fixed, price_donation, price_discount,
-            occupancy=OCCUPANCY,
-            location_number=LOCATION_NO,
-            tract,
-            linc_tier_type=LINC_TIER_TYPE,
-            net_after_assistance, bill_before_assistance, discount_assistance, crisis_voucher_amount,
-            senior_disabilities,
-            writeoff_amount, collection_sent_amount, collection_collected_amount,
-            final_payment, final_writeoff,
-            agg,
-            delinquent)
-
-portland_panel_estimation <- rbind(portland_panel_estimation_old,
-                                   portland_panel_estimation) %>%
-  distinct()
-
-portland_panel_2024q1 <- portland_panel_estimation %>%
-  filter(year(bill_date)==2024, quarter(bill_date)==1)
-
-portland_panel_2024q1 %>%
-  select(account_number) %>%
+portland_panel_2024q2 %>%
+  select(tu_id) %>%
   distinct() %>%
-  count() #148,467
+  count() #149,328
 
-portland_panel_2024q1 <- portland_panel_2024q1 %>%
+portland_panel_2024q2 <- portland_panel_2024q2 %>%
   filter(type_code=="REGLR")
 
-portland_panel_2024q1 %>%
-  select(account_number) %>%
+portland_panel_2024q2 %>%
+  select(tu_id) %>%
   distinct() %>%
-  count() #143,255
+  count() #146,063
 
-portland_panel_2024q1 %>%
+portland_panel_seniors <- portland_panel_2024q2 %>%
   group_by(linc_tier_type, senior_disabilities, delinquent) %>%
-  summarise(n=n_distinct(account_number))
+  summarise(n=n_distinct(tu_id)) %>%
+  ungroup()
 
-portland_panel_2024q1 <- portland_panel_2024q1 %>%
+portland_panel_seniors <- portland_panel_seniors %>%
+  spread(delinquent, n) %>%
+  mutate(n_account=`FALSE`+`TRUE`,
+         n_delinquent=`TRUE`) %>%
+  select(-`TRUE`, -`FALSE`) %>%
+  bind_rows(summarise(., 
+                      across(where(is.numeric), sum),
+                      across(where(is.character), ~'Total'))) %>%
+  mutate(share_delinquent=n_delinquent/n_account*100)
+
+tab <- TexRow(c("Assistance", 
+                "Seniors and Disabled",
+                "# Accounts", 
+                "# Delinquent Accounts",
+                "Share")) +
+  TexMidrule() +
+  TexRow(c("Tier 1", "Yes")) /
+  TexRow(portland_panel_seniors[2, 3:5] %>% as.numeric(),
+         dec=c(0, 0, 1), percentage=c(FALSE, FALSE, TRUE)) +
+  TexRow(c("", "No")) /
+  TexRow(portland_panel_seniors[1, 3:5] %>% as.numeric(),
+         dec=c(0, 0, 1), percentage=c(FALSE, FALSE, TRUE)) +
+  TexRow(c("Tier 2", "Yes")) /
+  TexRow(portland_panel_seniors[4, 3:5] %>% as.numeric(),
+         dec=c(0, 0, 1), percentage=c(FALSE, FALSE, TRUE)) +
+  TexRow(c("", "No")) /
+  TexRow(portland_panel_seniors[3, 3:5] %>% as.numeric(),
+         dec=c(0, 0, 1), percentage=c(FALSE, FALSE, TRUE)) +
+  TexRow(c("No Assistance", "N/A")) /
+  TexRow(portland_panel_seniors[5, 3:5] %>% as.numeric(),
+         dec=c(0, 0, 1), percentage=c(FALSE, FALSE, TRUE)) +
+  TexRow(c("Total", "N/A")) /
+  TexRow(portland_panel_seniors[6, 3:5] %>% as.numeric(),
+         dec=c(0, 0, 1), percentage=c(FALSE, FALSE, TRUE))
+  
+TexSave(tab, filename="fa_honored", positions=rep('c', 5),
+        output_path=output_dir, stand_alone=FALSE)
+
+portland_panel_2024q2 <- portland_panel_2024q2 %>%
   filter(!senior_disabilities)
 
-portland_panel_2024q1 %>%
-  select(account_number) %>%
+portland_panel_2024q2 %>%
+  select(tu_id) %>%
   distinct() %>%
   count() #141,249
 
 # Descriptive on accounts
 delinquent_threshold <- function(d) {
-  portland_panel_2024q1 %>%
+  portland_panel_2024q2 %>%
     mutate(delinquent=leftover_debt>d) %>%
     group_by(linc_tier_type, delinquent) %>%
-    summarise(n=n_distinct(account_number))
+    summarise(n=n_distinct(tu_id))
 }
 
 delinquent_threshold(0)
@@ -97,21 +101,21 @@ delinquent_threshold(10)
 delinquent_threshold(50)
 delinquent_threshold(115)
 
-portland_panel_2024q1 <- portland_panel_estimation %>%
-  filter(account_number %in% portland_panel_2024q1$account_number)
+portland_panel_2024q2 <- portland_panel_estimation %>%
+  filter(tu_id %in% portland_panel_2024q2$tu_id)
 
 delinquent_threshold <- function(d) {
-  portland_panel_2024q1 %>%
+  portland_panel_2024q2 %>%
     mutate(delinquent=leftover_debt>d) %>%
-    arrange(account_number, bill_date) %>%
-    group_by(account_number) %>%
+    arrange(tu_id, bill_date) %>%
+    group_by(tu_id) %>%
     mutate(delinquent_lag=lag(delinquent, 1)) %>%
     ungroup() %>%
     mutate(delinquent_consistent=(delinquent & delinquent_lag)) %>%
     filter(year(bill_date)==2024, quarter(bill_date)==1,
            type_code=="REGLR", !senior_disabilities) %>%
     group_by(linc_tier_type, delinquent_consistent) %>%
-    summarise(n=n_distinct(account_number))
+    summarise(n=n_distinct(tu_id))
 }
 
 delinquent_threshold(0)
@@ -120,10 +124,10 @@ delinquent_threshold(50)
 delinquent_threshold(115)
 
 delinquent_threshold <- function(d) {
-  portland_panel_2024q1 %>%
+  portland_panel_2024q2 %>%
     mutate(delinquent=leftover_debt>d) %>%
-    arrange(account_number, bill_date) %>%
-    group_by(account_number) %>%
+    arrange(tu_id, bill_date) %>%
+    group_by(tu_id) %>%
     mutate(delinquent_lag=lag(delinquent, 1),
            delinquent_lag_lag=lag(delinquent, 2)) %>%
     ungroup() %>%
@@ -131,7 +135,7 @@ delinquent_threshold <- function(d) {
     filter(year(bill_date)==2024, quarter(bill_date)==1,
            type_code=="REGLR", !senior_disabilities) %>%
     group_by(linc_tier_type, delinquent_consistent) %>%
-    summarise(n=n_distinct(account_number))
+    summarise(n=n_distinct(tu_id))
 }
 
 delinquent_threshold(0)
@@ -141,31 +145,31 @@ delinquent_threshold(115)
 
 # Non-consecutive missers
 portland_panel_estimation %>%
-  arrange(account_number, bill_date) %>%
-  group_by(account_number) %>%
+  arrange(tu_id, bill_date) %>%
+  group_by(tu_id) %>%
   mutate(delinquent_lag=lag(delinquent, 1)) %>%
   ungroup() %>%
   mutate(delinquent_once=(delinquent & !delinquent_lag)) %>%
   filter(type_code=="REGLR") %>%
-  group_by(account_number, bill_year) %>%
+  group_by(tu_id, bill_year) %>%
   summarise(delinquent=sum(delinquent, na.rm=TRUE),
             delinquent_once=sum(delinquent_once, na.rm=TRUE)) %>%
   ungroup() %>%
   mutate(delinquent=delinquent>0,
          delinquent_once=delinquent_once>0) %>%
   group_by(delinquent, delinquent_once, bill_year) %>%
-  summarise(n=n_distinct(account_number))
+  summarise(n=n_distinct(tu_id))
 
 portland_panel_estimation %>%
-  arrange(account_number, bill_date) %>%
-  group_by(account_number) %>%
+  arrange(tu_id, bill_date) %>%
+  group_by(tu_id) %>%
   filter(any(bill_year==2021),
          any(bill_year==2024)) %>%
   mutate(delinquent_lag=lag(delinquent, 1)) %>%
   ungroup() %>%
   mutate(delinquent_once=(delinquent & !delinquent_lag)) %>%
   filter(type_code=="REGLR") %>%
-  group_by(account_number, bill_year) %>%
+  group_by(tu_id, bill_year) %>%
   summarise(delinquent=sum(delinquent, na.rm=TRUE),
             delinquent_once=sum(delinquent_once, na.rm=TRUE)) %>%
   ungroup() %>%
@@ -173,16 +177,16 @@ portland_panel_estimation %>%
          delinquent_once=delinquent==delinquent_once & delinquent>0,
          delinquent_more=delinquent>delinquent_once & delinquent>0) %>%
   group_by(never_delinquent, delinquent_once, delinquent_more) %>%
-  summarise(n=n_distinct(account_number))
+  summarise(n=n_distinct(tu_id))
 
 # Financial assistance
-portland_panel_2024q1 <- portland_panel_estimation %>%
+portland_panel_2024q2 <- portland_panel_estimation %>%
   filter(year(bill_date)==2024, quarter(bill_date)==1, type_code=="REGLR")
 
-portland_panel_2024q1_fa <- portland_panel_2024q1 %>%
+portland_panel_2024q2_fa <- portland_panel_2024q2 %>%
   filter(!is.na(linc_tier_type), !senior_disabilities)
 
-portland_panel_2024q1_fa_summary <- portland_panel_2024q1_fa %>%
+portland_panel_2024q2_fa_summary <- portland_panel_2024q2_fa %>%
   mutate(discount_rate=discount_assistance*(-1)/bill_before_assistance,
          discount_rate=case_when(discount_rate<0 ~ 0,
                                  discount_rate>1 ~ 1,
@@ -203,5 +207,5 @@ portland_panel_2024q1_fa_summary <- portland_panel_2024q1_fa %>%
             sd_discount_rate=sd(discount_rate, na.rm=TRUE),
             min_discount_rate=min(discount_rate, na.rm=TRUE),
             max_discount_rate=max(discount_rate, na.rm=TRUE),
-            n=n_distinct(account_number)) %>%
+            n=n_distinct(tu_id)) %>%
   ungroup()
